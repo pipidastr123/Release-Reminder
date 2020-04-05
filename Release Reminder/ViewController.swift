5740d5f84b10bb3dc4630e21e5b147dfb3be5a01
Andrei Zhuravlev <dwasdx@gmail.com>
2020-04-05 18:48:24 +0300

    Initial Commit

---
+++ Release Reminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Release Reminder
+//
+//  Created by Андрей Журавлев on 05.04.2020.
+//  Copyright © 2020 Андрей Журавлев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
